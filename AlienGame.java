f0c6220c4c5a692063192e0ab326c7a20ee7f4bd
Ogerator <oskar.soderberg@live.com>
2015-01-25 15:43:42 +0100

    Nu skuter den vilt

--- AlienGame.java
+++ AlienGame.java
@@ -11,27 +11,33 @@ import java.awt.event.KeyListener;
 
 public class AlienGame extends JComponent implements ActionListener, KeyListener {
 
-
-    //Bullet[] bullets = Bullet[10];
-    Bullet theBullet = new Bullet();
-    int shipX = 400-15;
+    Bullet[] bullets = new Bullet[10];
+    // Bullet theBullet = new Bullet();
     int windowX = 800;
-    int windowY = 600;
+    public int windowY = 600;
     int shipSizeX = 30;
     int shipSizeY = 10;
+	public int shipX = windowX / 2 - shipSizeX / 2;
     int shipSpeedRight = 0;
     int shipSpeedLeft = 0;
     int shipSpeed = 2; //2 pixlar per 10 ms
-    boolean debugMode = true;
-
+    boolean debugMode = false;
+	int nextBullet;
+	
+	public AlienGame() {
+		for (int i = 0; i < 10; i++) {
+			bullets[i] = new Bullet();
+		}
+	}
     public static void main (String[] arg) {
         AlienGame theClass = new AlienGame();
         try{
             if(arg[0].equals("debug")){
                theClass.debugModeOn();
+			   System.out.println("[info] Running in debug mode");
             }
         }catch (Exception e){
-            if(theClass.debugCheck()==true){
+            if(theClass.debugCheck()){
                 System.err.println("[ ex ] "+e);
             }
         }
@@ -43,7 +49,7 @@ public class AlienGame extends JComponent implements ActionListener, KeyListener
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setLocationRelativeTo(null);
         window.setVisible(true);
-        if(theClass.debugCheck()==true){
+        if(theClass.debugCheck()){
 
             System.out.println("[ ok ] All systems go");
         }
@@ -58,12 +64,18 @@ public class AlienGame extends JComponent implements ActionListener, KeyListener
 
     @Override
     public void paintComponent(Graphics g) {
-        g.setColor(new Color(0, 0, 0));
+        // Bakgrund
+		g.setColor(new Color(0, 0, 0));
         g.fillRect(0, 0, windowX, windowY);
 		
-		g.setColor(new Color(255, 255, 255));
+		// Skott
+		for (int i = 0; i < 10;  i++) {
+			g.setColor(new Color(255, 255, 255));
+			//if (bullets[i].x != null && bullets[i].y != null)
+				g.fillRect(bullets[i].x, bullets[i].y, 2, 10);
+		}
 		
-		
+		// Ritar rymdskepp
         g.setColor(new Color(255, 2, 57));
         g.fillRect(shipX, windowY - 100, shipSizeX, shipSizeY);
     }
@@ -72,22 +84,28 @@ public class AlienGame extends JComponent implements ActionListener, KeyListener
         if(shipX>=0&&shipX<=windowX-shipSizeX){
             shipX = shipX + shipSpeedRight + shipSpeedLeft;
         }else if(shipX<=0){
-            if(debugMode==true){
+            if(debugMode){
                 System.out.println("[info] left edge reached");
             }
             shipX++;
         }else if(shipX>=windowX-shipSizeX){
-            if(debugMode==true){
+            if(debugMode){
                 System.out.println("[info] right edge reached");
             }
             shipX--;
         }else{
-            if(debugMode==true){
+            if(debugMode){
                 System.err.println("[ ex ] Illegal position");
                 System.err.println(shipX);
                 System.exit(0);
             }
         }
+		// Skottet rör sig
+		for (int i = 0; i < 10; i++) {
+			if (bullets[i].y > 0) {
+				bullets[i].y = bullets[i].y - 3;
+			}
+		}
         repaint();
     }
 
@@ -99,6 +117,18 @@ public class AlienGame extends JComponent implements ActionListener, KeyListener
         if (e.getKeyCode() == KeyEvent.VK_LEFT) {
             shipSpeedLeft = -shipSpeed;
         }
+		if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+			bullets[nextBullet].x = shipX;
+			bullets[nextBullet].y = windowY - 100;
+			if (nextBullet + 1 < 10)
+				nextBullet++;
+			else
+				nextBullet = 0;
+				
+			if (debugMode) {
+				System.out.println("[actn] Space hit");
+			}
+		}
     }
 
     @Override
@@ -127,6 +157,6 @@ public class AlienGame extends JComponent implements ActionListener, KeyListener
 }
 
 class Bullet {
-    int x = 500;
+    int x;
     int y;
 }
